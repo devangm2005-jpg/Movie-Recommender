2b45c6d1a68968b244156adb71acf5821a91d65f
devangm2005-jpg <devangm2005@gmail.com>
2026-01-11 20:27:11 +0530

    app.py change

--- backend/app.py
+++ backend/app.py
@@ -28,7 +28,7 @@ def recommend_movie():
         print("Error:", e)
         return jsonify({"error": "Server error. Please try again later."}), 500
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     import os
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
